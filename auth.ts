76ba9c1ac62320776b86f223fb2f63528de06eda
Naguib <moma.naguib@gmail.com>
2025-02-27 15:48:25 +0100

    fix signin auth

--- auth.ts
+++ auth.ts
@@ -1,21 +1,82 @@
-import NextAuth from 'next-auth';
-import { PrismaAdapter } from '@auth/prisma-adapter';
-import { prisma } from '@/lib/prisma';
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/prisma";
+import { compare } from "bcryptjs";
+import type { User } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { Session } from "next-auth";
 
-import GoogleProvider from 'next-auth/providers/google';
-
+interface ExtendedSession extends Session {
+  accessToken?: string;
+}
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
       allowDangerousEmailAccountLinking: true,
+    }),
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        if (!email || !password) return null;
+        const user = await prisma.user.findUnique({
+          where: { email },
+        });
+        if (!user) return null;
+        const isPasswordValid = await compare(password, user.password);
+        if (!isPasswordValid) return null;
+        return user;
+      },
     }),
   ],
   pages: {
-    signIn: 'login',
-    error: '/auth/error',
+    signIn: "/auth/signin",
+    error: "/auth/error",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+  callbacks: {
+    async session({ session, token }): Promise<ExtendedSession> {
+      return {
+        ...session,
+        accessToken: token.accessToken as string,
+        user: {
+          ...session.user,
+          id: token.sub,
+        },
+      };
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        const u = user as User & { role: string };
+        return {
+          ...token,
+          id: u.id,
+          role: u.role,
+        };
+      }
+      return token;
+    },
   },
 });
